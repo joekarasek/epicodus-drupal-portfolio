269bf76a5629e24c1ef0af17f1fb25b646d5e5d7
Joe Karasek <joseph.karasek@gmail.com>
2016-05-07 11:44:13 -0700

    breakout is now in a block, woohoo

---
+++ sites/all/modules/custom_modules/breakout/scripts.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded",function() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  var x = canvas.width/2;
+  var y = canvas.height-30;
+  var dx = 2;
+  var dy = -2;
+  var ballRadius = 10;
+  var paddleHeight = 10;
+  var paddleWidth = 75;
+  var paddleX = (canvas.width-paddleWidth)/2;
+
+  var rightPressed = false;
+  var leftPressed = false;
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false)
+
+  function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+  }
+  function keyUpHandler(e) {
+      if(e.keyCode == 39) {
+          rightPressed = false;
+      }
+      else if(e.keyCode == 37) {
+          leftPressed = false;
+      }
+  }
+
+  function drawBall() {
+    // Draw the ball
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function draw() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawBall();
+    drawPaddle();
+
+// Detect Collisions
+    if (x > paddleX && x < paddleX + paddleWidth && y > canvas.height-ballRadius-paddleHeight) {
+      dy = -dy;
+      debugger;
+    }
+    if(y + dy < ballRadius) {
+      // Bounce off the top
+      dy = -dy;
+    } else if (y + dy > canvas.height-ballRadius) {
+      // Hit the bottom, game over
+      alert("Game Over");
+      document.location.reload();
+    }
+
+    // Bounce off the left and righ walls
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+      dx = -dx;
+    }
+
+    // Move the paddle
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    // Change the balls position
+    x += dx;
+    y += dy;
+  }
+
+  setInterval(draw, 10);
+
+})
